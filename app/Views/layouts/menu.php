e08608bb30654bd001fce8be23f04fa46d260296
Din <pudinmah69@gmail.com>
2025-04-08 16:04:17 +0700

    Membuat Menu Aktif dengan Js / jQuery di Template Bootstrap

--- app/Views/layouts/menu.php
+++ app/Views/layouts/menu.php
@@ -1,10 +1,10 @@
 <li class="menu-header">Main Menu</li>
-<li><a class="nav-link" href="<?=site_url()?>"><i class="fas fa-square"></i> <span>Dashboard</span></a></li>
-<li class="active"><a class="nav-link" href="<?=site_url('gawe')?>"><i class="fas fa-calendar"></i> <span>GAWE / Acara</span></a></li>
+<li><a class="nav-link" href="<?=site_url('home')?>"><i class="fas fa-square"></i> <span>Dashboard</span></a></li>
+<li><a class="nav-link" href="<?=site_url('gawe')?>"><i class="fas fa-calendar"></i> <span>GAWE / Acara</span></a></li>
 <li class="nav-item dropdown">
     <a href="#" class="nav-link has-dropdown"><i class="far fa-address-book"></i><span>Kontak</span></a>
     <ul class="dropdown-menu">
-        <li><a class="nav-link" href="index-0.html">Group Kontak</a></li>
+        <li><a class="nav-link" href="<?=site_url('groups')?>">Group Kontak</a></li>
         <li><a class="nav-link" href="index.html">Kontak Saya</a></li>
     </ul>
 </li>
