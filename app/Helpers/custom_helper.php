62f443f22aaa77f254d43a7203f25aa8a8862ae9
Din <pudinmah69@gmail.com>
2025-04-09 12:42:14 +0700

    Membuat Dashboard Keren Dinamis

--- app/Helpers/custom_helper.php
+++ app/Helpers/custom_helper.php
@@ -4,6 +4,7 @@ function userLogin() {
     return $db->table('users')->where('id_user', session('id_user'))->get()->getRow();
 }
 
+// menampilkan data dashboard
 function countData($table) {
     $db = \Config\Database::connect();
     return $db->table($table)->countAllResults();
--- app/Views/home.php
+++ app/Views/home.php
@@ -1,5 +1,3 @@
-<!-- Using Layouts in Views -->
-<!-- https://codeigniter.com/user_guide/outgoing/view_layouts.html#using-layouts-in-views -->
 <?= $this->extend('layouts/default') ?>
 
 <?= $this->section('title') ?>
@@ -10,12 +8,74 @@
 <?= $this->section('content') ?>
 <section class="section">
     <div class="section-header">
-        <h1><?= $submenu ?></h1>
+        <h1>Dashboard</h1>
     </div>
 
     <div class="section-body">
-        Halo Dunia
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-primary">
+                        <i class="far fa-user"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Gawe</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= countData('gawe') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-danger">
+                        <i class="far fa-newspaper"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Grup</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= countData('groups') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-warning">
+                        <i class="far fa-file"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Kontak</h4>
+                        </div>
+                        <div class="card-body">
+                            <?= countData('contacts') ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                <div class="card card-statistic-1">
+                    <div class="card-icon bg-success">
+                        <i class="fas fa-circle"></i>
+                    </div>
+                    <div class="card-wrap">
+                        <div class="card-header">
+                            <h4>Undangan</h4>
+                        </div>
+                        <div class="card-body">
+                            47
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 </section>
-
 <?= $this->endSection() ?>
